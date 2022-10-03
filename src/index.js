981ece52a898889ce591942d24b528d576b6ada8
Oleg Palashynskyi <palah@ukr.net>
2022-10-03 20:20:47 +0300

    add Profile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+
 import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Profile
+//     username={user.username}
+//     tag={user.tag}
+//     location={user.location}
+//     avatar={user.avatar}
+//     stats={user.stats}
+//   />
+// );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
